0ad33759702e1f633daf43e316bf65488c56b15e
aghiljv <aghiljv@gmail.com>
2026-09-25 20:59:22 +0200

    about page test

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -38,12 +38,11 @@ defineOgImage('Portfolio', { title, description })
         links: 'justify-start'
       }"
     >
-      <UColorModeAvatar
-        class="sm:rotate-4 size-36 rounded-lg ring ring-default ring-offset-3 ring-offset-bg"
-        :light="global.picture?.light!"
-        :dark="global.picture?.dark!"
-        :alt="global.picture?.alt!"
-      />
+<img
+          :src="global.picture?.dark"
+          :alt="global.picture?.alt"
+          class="sm:rotate-4 size-36 rounded-lg ring ring-default ring-offset-3 ring-offset-bg"
+        >
     </UPageHero>
     <UPageSection
       :ui="{
